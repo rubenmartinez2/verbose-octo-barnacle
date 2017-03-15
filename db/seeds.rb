f9c1d75c5e8811f1d2be697892096cfe5b5895ef
Ruben Martinez <mruben922@gmail.com>
2017-03-15 18:21:10 +0000

    first

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,48 +8,33 @@
 #---
 # encoding: utf-8
 Product.delete_all
-Product.create(title: 'Rails, Angular, Postgres, and Bootstrap',
+Product.create(title: 'Asus Desktop',
   description:
     %{<p>
-      <em>Powerful, Effective, and Efficient Full-Stack Web Development</em>
-      As a Rails developer, you care about user experience and performance,
-      but you also want simple and maintainable code. Achieve all that by
-      embracing the full stack of web development, from styling with
-      Bootstrap, building an interactive user interface with AngularJS, to
-      storing data quickly and reliably in PostgreSQL. Take a holistic view of
-      full-stack development to create usable, high-performing applications,
-      and learn to use these technologies effectively in a Ruby on Rails
-      environment.
+      <em>Powerful, Effective, and Efficient with a Nvidia gtx 1070</em>
+       Stay productive with this Asus desktop PC. With 16GB of RAM and
+       an Intel Core i7 processor. 
       </p>},
-  image_url: 'dcbang.jpg',    
-  price: 45.00)
+  image_url: 'Asus comput gtx 1070.jpg',    
+  price: 1000.00)
 # . . .
-Product.create(title: 'Seven Mobile Apps in Seven Weeks',
+Product.create(title: 'Alien Ware Area51 Desktop',
   description:
     %{<p>
-      <em>Native Apps, Multiple Platforms</em>
-      Answer the question “Can we build this for ALL the devices?” with a
-      resounding YES. This book will help you get there with a real-world
-      introduction to seven platforms, whether you’re new to mobile or an
-      experienced developer needing to expand your options. Plus, you’ll find
-      out which cross-platform solution makes the most sense for your needs.
+      <em>Area 51 Gaming Computer</em>
+      ALienware Area 51 desktop computer. It achieves superior thanks to it's
+      hexa-core Intel i& processor and 32GB of DDR4 RAM, and 2TB memory.
       </p>},
-  image_url: '7apps.jpg',
-  price: 26.00)
+  image_url: 'area51.jpg',
+  price: 1200.00)
 # . . .
 
-Product.create(title: 'Ruby Performance Optimization',
+Product.create(title: 'Razorblade Laptop',
   description:
     %{<p>
-      <em>Why Ruby Is Slow, and How to Fix It</em> 
-      You don’t have to accept slow Ruby or Rails performance. In this
-      comprehensive guide to Ruby optimization, you’ll learn how to write
-      faster Ruby code—but that’s just the beginning. See exactly what makes
-      Ruby and Rails code slow, and how to fix it. Alex Dymo will guide you
-      through perils of memory and CPU optimization, profiling, measuring,
-      performance testing, garbage collection, and tuning. You’ll find that
-      all those “hard” things aren’t so difficult after all, and your code
-      will run orders of magnitude faster.
+      <em>Why Razorblade Stealth</em> 
+      Blade Stealth computer. The 2.7 Ghz Intel Core i7 processor delivers fast speeds
+      for on-the-go productivity and streaming video, and the 512GB SSD. 
       </p>},
-  image_url: 'adrpo.jpg',
-  price: 46.00)
+  image_url: 'razorblade.jpg',
+  price: 1500.00)
